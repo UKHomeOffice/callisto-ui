bad66bafa627be57af6737bbc051abf8d96362ed
Holly Blues <holly.blues@digital.homeoffice.gov.uk>
2022-03-15 17:26:57 +0000

    EAHW-1717: Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import useGetExample from "./api/getExample";
+import Footer from "./components/layout/footer/Footer";
 import Header from "./components/layout/header/Header";
 import Home from "./pages/Home";
 
@@ -13,11 +14,16 @@ function App() {
   return (
     <div className='App'>
       <Header />
-      <div className='govuk-main-wrapper'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
+      <div id='main-content' className='govuk-main-wrapper'>
+        <div className='govuk-width-container '>
+          <main className='govuk-main-wrapper ' id='main-content' role='main'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+            </Routes>
+          </main>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
