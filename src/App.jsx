ae3c868dfac323bb96d51c89de10d4eda6d6e7d4
Holly Blues <holly.blues@digital.homeoffice.gov.uk>
2022-03-23 14:28:48 +0000

    EAHW-1717: Eslint setup and format changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import useGetExample from "./api/getExample";
-import Footer from "./components/layout/footer/Footer";
-import Header from "./components/layout/header/Header";
-import Home from "./pages/Home";
+import { Routes, Route } from 'react-router-dom'
+import Footer from './components/layout/footer/Footer'
+import Header from './components/layout/header/Header'
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const { data, error, loading } = useGetExample();
-
   return (
-    <div className='App'>
+    <div className="App">
       <a
-        href='#main-content'
-        className='govuk-skip-link'
-        data-module='govuk-skip-link'
+        href="#main-content"
+        className="govuk-skip-link"
+        data-module="govuk-skip-link"
       >
         Skip to main content
       </a>
       <Header />
-      <div className='govuk-width-container '>
-        <main className='govuk-main-wrapper ' id='main-content' role='main'>
+      <div className="govuk-width-container ">
+        <main className="govuk-main-wrapper " id="main-content" role="main">
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </main>
       </div>
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
