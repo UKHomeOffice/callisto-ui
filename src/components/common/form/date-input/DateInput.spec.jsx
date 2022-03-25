9433c7cece3f0ae9d4f3c488c7172f04e2c460bd
joannafawl <joanna.fawl@madetech.com>
2022-03-25 15:31:05 +0000

    WIP add basic forms page to demo components

--- src/components/common/form/date-input/DateInput.spec.jsx
+++ src/components/common/form/date-input/DateInput.spec.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { screen, render } from "@testing-library/react";
+import React from 'react'
+import { screen, render, fireEvent } from '@testing-library/react'
 
-import DateInput from "./DateInput";
+import DateInput from './DateInput'
 
-describe("DateInput", () => {
-  it("should render a date input component with the correct heading", () => {
+describe('DateInput', () => {
+  it('should render a date input component with the correct heading', () => {
     render(
       <DateInput name="test" heading="What is the date?" headingSize="l" />
-    );
+    )
 
-    const heading = screen.getByText("What is the date?");
+    const heading = screen.getByText('What is the date?')
 
-    expect(heading).toBeTruthy();
-  });
+    expect(heading).toBeTruthy()
+  })
 
-  it("should render a date input component with the correct hint", () => {
+  it('should render a date input component with the correct hint', () => {
     render(
       <DateInput
         name="test"
@@ -22,80 +22,110 @@ describe("DateInput", () => {
         headingSize="l"
         hint="eg. 01/01/2022"
       />
-    );
+    )
 
-    const hint = screen.getByText("eg. 01/01/2022");
+    const hint = screen.getByText('eg. 01/01/2022')
 
-    expect(hint).toBeTruthy();
-  });
+    expect(hint).toBeTruthy()
+  })
 
-  describe("Error messages", () => {
-    it("should display all error messages", () => {
+  it('should update the input value on change', () => {
+    const handleDayChangeMock = jest.fn()
+    const handleMonthChangeMock = jest.fn()
+    const handleYearChangeMock = jest.fn()
+
+    render(
+      <DateInput
+        name="test"
+        heading="What is the date?"
+        headingSize="l"
+        hint="eg. 01/01/2022"
+        handleDayChange={(event) => handleDayChangeMock(event)}
+        handleMonthChange={(event) => handleMonthChangeMock(event)}
+        handleYearChange={(event) => handleYearChangeMock(event)}
+      />
+    )
+
+    const dayInput = screen.getByTestId('day-input')
+    const monthInput = screen.getByTestId('month-input')
+    const yearInput = screen.getByTestId('year-input')
+
+    fireEvent.change(dayInput, { target: { value: '01' } })
+    fireEvent.change(monthInput, { target: { value: '03' } })
+    fireEvent.change(yearInput, { target: { value: '2022' } })
+
+    expect(dayInput.value).toBe('01')
+    expect(monthInput.value).toBe('03')
+    expect(yearInput.value).toBe('2022')
+  })
+
+  describe('Error messages', () => {
+    it('should display all error messages', () => {
       render(
         <DateInput
           name="test"
           heading="What is the date?"
           headingSize="l"
           errors={[
-            { inputName: "test", message: "Date cannot be blank" },
-            { inputName: "test-day", message: "Enter a day" },
-            { inputName: "test-month", message: "Enter a month" },
-            { inputName: "test-year", message: "Enter a year" },
+            { inputName: 'test', message: 'Date cannot be blank' },
+            { inputName: 'test-day', message: 'Enter a day' },
+            { inputName: 'test-month', message: 'Enter a month' },
+            { inputName: 'test-year', message: 'Enter a year' },
           ]}
         />
-      );
+      )
 
-      const overallErrorMessage = screen.getByText("Date cannot be blank");
-      const dayErrorMessage = screen.getByText("Enter a day");
-      const monthErrorMessage = screen.getByText("Enter a month");
-      const yearErrorMessage = screen.getByText("Enter a year");
+      const overallErrorMessage = screen.getByText('Date cannot be blank')
+      const dayErrorMessage = screen.getByText('Enter a day')
+      const monthErrorMessage = screen.getByText('Enter a month')
+      const yearErrorMessage = screen.getByText('Enter a year')
 
-      expect(overallErrorMessage).toBeTruthy();
-      expect(dayErrorMessage).toBeTruthy();
-      expect(monthErrorMessage).toBeTruthy();
-      expect(yearErrorMessage).toBeTruthy();
-    });
+      expect(overallErrorMessage).toBeTruthy()
+      expect(dayErrorMessage).toBeTruthy()
+      expect(monthErrorMessage).toBeTruthy()
+      expect(yearErrorMessage).toBeTruthy()
+    })
 
-    it("should add error styling to day input box", () => {
+    it('should add error styling to day input box', () => {
       render(
         <DateInput
           name="test"
           heading="What is the date?"
           headingSize="l"
-          errors={[{ inputName: "test-day", message: "Enter a day" }]}
+          errors={[{ inputName: 'test-day', message: 'Enter a day' }]}
         />
-      );
+      )
 
-      const dayInputBox = screen.getByTestId("day-input");
-      expect(dayInputBox.className).toContain("govuk-input--error");
-    });
+      const dayInputBox = screen.getByTestId('day-input')
+      expect(dayInputBox.className).toContain('govuk-input--error')
+    })
 
-    it("should add error styling to month input box", () => {
+    it('should add error styling to month input box', () => {
       render(
         <DateInput
           name="test"
           heading="What is the date?"
           headingSize="l"
-          errors={[{ inputName: "test-month", message: "Enter a month" }]}
+          errors={[{ inputName: 'test-month', message: 'Enter a month' }]}
         />
-      );
+      )
 
-      const monthInputBox = screen.getByTestId("month-input");
-      expect(monthInputBox.className).toContain("govuk-input--error");
-    });
+      const monthInputBox = screen.getByTestId('month-input')
+      expect(monthInputBox.className).toContain('govuk-input--error')
+    })
 
-    it("should add error styling to year input box", () => {
+    it('should add error styling to year input box', () => {
       render(
         <DateInput
           name="test"
           heading="What is the date?"
           headingSize="l"
-          errors={[{ inputName: "test-year", message: "Enter a year" }]}
+          errors={[{ inputName: 'test-year', message: 'Enter a year' }]}
         />
-      );
+      )
 
-      const yearInputBox = screen.getByTestId("year-input");
-      expect(yearInputBox.className).toContain("govuk-input--error");
-    });
-  });
-});
+      const yearInputBox = screen.getByTestId('year-input')
+      expect(yearInputBox.className).toContain('govuk-input--error')
+    })
+  })
+})
---
+++ src/pages/FormsExample.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from 'react'
+import DateInput from '../components/common/form/date-input/DateInput'
+import ErrorSummary from '../components/common/form/error-summary/ErrorSummary'
+import Input from '../components/common/form/input/Input'
+import Radios from '../components/common/form/radios/Radios'
+
+const FormsExample = () => {
+  const [name, setName] = useState('')
+  const [location, setLocation] = useState('')
+  const [day, setDay] = useState('')
+  const [month, setMonth] = useState('')
+  const [year, setYear] = useState('')
+  const [errors, setErrors] = useState([])
+  const [submitted, setSubmitted] = useState(false)
+
+  const handleSubmit = () => {
+    setSubmitted(true)
+    validateInput()
+  }
+
+  const handleFormChange = (event) => {
+    setName(event.target.value)
+  }
+
+  const handleRadiosChange = (event) => {
+    setLocation(event.target.value)
+  }
+
+  const handleDayChange = (event) => {
+    setDay(event.target.value)
+  }
+
+  const handleMonthChange = (event) => {
+    setMonth(event.target.value)
+  }
+
+  const handleYearChange = (event) => {
+    setYear(event.target.value)
+  }
+
+  const validateInput = async () => {
+    const errorsArray = []
+    if (location === '') {
+      errorsArray.push({
+        inputName: 'whereDoYouLive',
+        message: 'Select a location',
+      })
+    }
+    if (name === '') {
+      errorsArray.push({
+        inputName: 'name',
+        message: 'Enter a name',
+      })
+    }
+    if (day === '') {
+      errorsArray.push({
+        inputName: 'dateOfBirth-day',
+        message: 'Day cannot be blank',
+      })
+    }
+    if (month === '') {
+      errorsArray.push({
+        inputName: 'dateOfBirth-month',
+        message: 'Month cannot be blank',
+      })
+    }
+    if (year === '') {
+      errorsArray.push({
+        inputName: 'dateOfBirth-year',
+        message: 'Year cannot be blank',
+      })
+    }
+    setErrors(errorsArray)
+  }
+
+  return (
+    <>
+      <h1 className="govuk-heading-xl">Forms example page</h1>
+      {submitted && errors.length > 0 && <ErrorSummary errors={errors} />}
+
+      <Input
+        name="name"
+        heading="What is your name?"
+        headingSize="m"
+        inputWidth="20"
+        hint="eg. Joanna"
+        errors={errors}
+        value={name}
+        handleFormChange={(event) => handleFormChange(event)}
+      />
+
+      <Radios
+        name="whereDoYouLive"
+        heading="Where do you live?"
+        headingSize="m"
+        options={['England', 'Ireland', 'Scotland', 'Wales']}
+        hint="eg. England"
+        errors={errors}
+        value={location}
+        handleRadiosChange={(event) => handleRadiosChange(event)}
+      />
+
+      <DateInput
+        name="dateOfBirth"
+        heading="What is your date of birth?"
+        headingSize="m"
+        hint="eg. 01/01/1990"
+        errors={errors}
+        handleDayChange={(event) => handleDayChange(event)}
+        handleMonthChange={(event) => handleMonthChange(event)}
+        handleYearChange={(event) => handleYearChange(event)}
+      />
+
+      <button
+        className="govuk-button"
+        data-module="govuk-button"
+        onClick={() => {
+          handleSubmit()
+        }}
+      >
+        Save and continue
+      </button>
+    </>
+  )
+}
+
+export default FormsExample
