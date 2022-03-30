b9e7603e861d2d7e6492f6081f6b19b2bf9aea68
Xeena-Newman <xeena.newman@digital.homeoffice.gov.uk>
2022-03-30 15:33:16 +0100

    changed character limit to 250

--- src/components/common/form/text-area/Notes.jsx
+++ src/components/common/form/text-area/Notes.jsx
@@ -6,20 +6,20 @@ const Notes = () => {
       <div
         className="govuk-character-count"
         data-module="govuk-character-count"
-        data-maxlength="200"
+        data-maxlength="250"
       >
         {/* add --error to class when javascript is working */}
         <div className="govuk-form-group">
-          {/* <h1 className="govuk-label-wrapper">
-              <label className="govuk-label govuk-label--l" htmlFor="with-hint">
-                Notes
-              </label>
-            </h1> */}
+          <h1 className="govuk-label-wrapper">
+            <label className="govuk-label govuk-label--l" htmlFor="with-hint">
+              Notes
+            </label>
+          </h1>
 
-          {/* <p id="exceeding-characters-error" className="govuk-error-message">
-              <span className="govuk-visually-hidden">Error:</span> Notes must be
-              200 characters or fewer
-            </p> */}
+          <p id="exceeding-characters-error" className="govuk-error-message">
+            <span className="govuk-visually-hidden">Error:</span> Notes must be
+            250 characters or fewer
+          </p>
           <textarea
             className="govuk-textarea govuk-!-width-two-thirds govuk-js-character-count"
             id="with-hint"
@@ -34,7 +34,7 @@ const Notes = () => {
           className="govuk-hint govuk-character-count__message govuk-!-padding-bottom-4"
           aria-live="polite"
         >
-          You can enter up to 200 characters
+          You can enter up to 250 characters
         </div>
         <ButtonGroup buttonText1="Save" buttonText2="Cancel" />
       </div>
