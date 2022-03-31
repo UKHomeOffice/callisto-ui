ffe15149349a378daf1c0e130a3696753bf12aa9
Xeena-Newman <xeena.newman@digital.homeoffice.gov.uk>
2022-03-31 09:22:37 +0100

    removed summarylist component from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import SummaryList from './components/common/form/summary-list/SummaryList';
 import Footer from './components/layout/footer/Footer';
 import Header from './components/layout/header/Header';
 import FormsExample from './pages/FormsExample';
@@ -18,11 +17,6 @@ function App() {
       <Header />
       <div className="govuk-width-container ">
         <main className="govuk-main-wrapper " id="main-content" role="main">
-          <SummaryList
-            shiftType="Early shift"
-            hours="08:00 to 16:00"
-            mealBreak="30 minutes taken"
-          />
           <Routes>
             <Route path="/forms" element={<FormsExample />} />
             <Route path="/" element={<Home />} />
