11f29205d642e5f333877f5c920e7aa17d31421e
Xeena-Newman <xeena.newman@digital.homeoffice.gov.uk>
2022-04-06 10:48:31 +0100

    added name prop to test

--- src/components/common/form/checkbox/checkbox.spec.jsx
+++ src/components/common/form/checkbox/checkbox.spec.jsx
@@ -3,14 +3,24 @@ import '@testing-library/jest-dom';
 import Checkbox from './Checkbox';
 
 test('text is showing on page', () => {
-  render(<Checkbox text="Shift longer than 24 hours" />);
+  render(
+    <Checkbox
+      text="Shift longer than 24 hours"
+      name="shift-is-longer-than-24-hours"
+    />
+  );
 
   const text = screen.getByText('Shift longer than 24 hours');
   expect(text).toBeInTheDocument();
 });
 
 test('should set the checkbox to checked when it is selected', () => {
-  render(<Checkbox text="Shift longer than 24 hours" />);
+  render(
+    <Checkbox
+      text="Shift longer than 24 hours"
+      name="shift-is-longer-than-24-hours"
+    />
+  );
 
   const checkbox = screen.getByRole('checkbox');
   fireEvent.click(checkbox);
