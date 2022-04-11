d2e5e698d4aca753f8a7f34a1bc47376ea795a4a
Xeena-Newman <xeena.newman@digital.homeoffice.gov.uk>
2022-04-11 11:06:01 +0100

    copied files from prev Notes branch

---
+++ src/components/common/form/text-area/Notes.jsx
@@ -0,0 +1,42 @@
+import ButtonGroup from '../navigation/buttonGroup';
+
+const Notes = () => {
+  return (
+    <>
+      <div
+        className="govuk-character-count"
+        data-module="govuk-character-count"
+        data-maxlength="250"
+        id="text-area"
+      >
+        {/* add --error to form class when javascript is working and implement error for when charcter count in exceeded  */}
+        <div className="govuk-form-group">
+          {/* <p id="exceeding-characters-error" className="govuk-error-message">
+            <span className="govuk-visually-hidden">Error:</span> Notes must be
+            250 characters or fewer
+          </p> */}
+          <textarea
+            className="govuk-textarea govuk-!-width-two-thirds govuk-js-character-count"
+            id="with-hint"
+            name="with-hint"
+            rows="5"
+            aria-describedby="with-hint-info with-hint-hint"
+          ></textarea>
+        </div>
+
+        <div
+          id="with-hint-info"
+          className="govuk-hint govuk-character-count__message govuk-!-padding-bottom-4"
+          aria-live="polite"
+        >
+          You can enter up to 250 characters
+        </div>
+        <ButtonGroup buttonText1="Save" buttonText2="Cancel" />
+      </div>
+    </>
+  );
+};
+
+Notes.propTypes = {};
+
+export default Notes;
