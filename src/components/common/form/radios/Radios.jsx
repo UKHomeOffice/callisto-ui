2c99724aacfd60f797f748bd52714fc72f6ed17d
Xeena-Newman <xeena.newman@digital.homeoffice.gov.uk>
2022-04-19 15:03:34 +0100

    error disappears if characters return to below maxlength

--- src/components/common/form/radios/Radios.jsx
+++ src/components/common/form/radios/Radios.jsx
@@ -9,7 +9,9 @@ const Radios = React.forwardRef(
     return (
       <>
         <div
-          className={`govuk-form-group ${errors && errors[name] && 'govuk-form-group--error'}`}
+          className={`govuk-form-group ${
+            errors && errors[name] && 'govuk-form-group--error'
+          }`}
           data-testid="radio-buttons"
         >
           <fieldset
