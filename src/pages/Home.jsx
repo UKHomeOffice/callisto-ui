751d932cefcd0902286630835b5307d217595090
Holly Blues <holly.blues@digital.homeoffice.gov.uk>
2022-04-20 14:44:00 +0100

    EAHW-1859: Logic moved to App.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,4 @@
 const Home = () => {
-
-  const { initialized, keycloak } = useKeycloak();
-
-  useEffect(() => {
-    if (initialized && !keycloak.authenticated){
-      keycloak.login();
-    }
-  });
-
   return <h1 className="govuk-heading-xl">Home page</h1>;
 };
 
