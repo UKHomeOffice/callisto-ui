2f3f95805c9d088c11786051ecb84e57eedbc77d
laurie.bennett <laurie.bennett@digital.homeoffice.gov.uk>
2022-05-04 14:39:20 +0100

    EAHW-1249 added ability to show and hide detailed view

--- src/components/common/form/accruals-bucket/AccrualsBucket.jsx
+++ src/components/common/form/accruals-bucket/AccrualsBucket.jsx
@@ -1,27 +1,58 @@
+import React from 'react';
+
 const AccrualsBucket = ({
     bucketTitle,
+    bucketValue,
     bucketInfo
   }) => {
 
+    var showDetailedViewNonState = false;
+
+    const [state, setState] = React.useState({
+        showDetailedView: false
+    })
+
+    const toggleDetailedView = () => {
+        console.log("here");
+        const _state = {...state};
+        let showDetailedView = _state.showDetailedView;
+        showDetailedView = !showDetailedView;
+        showDetailedViewNonState = showDetailedView;
+        setState({ ..._state, showDetailedView});
+    }
+
     return (
         <div class="accruals-bucket__container">
-            <h2 class="accruals-bucket__title">
-                {bucketTitle}    
-            </h2>
+            <div className="accruals-bucket__title-container">
+                <h2 className="govuk-body-m accruals-bucket__title">
+                    {bucketTitle}    
+                </h2>
+                {bucketValue &&
+                    <div className="govuk-body-m accruals-bucket__value"> 
+                        {bucketValue}
+                    </div>
+                }
+            </div>
+            {showDetailedViewNonState &&
+                <img src="./src/assets/images/caret-down.png" className="accruals-bucket__hide-detailed-view" onClick={() => toggleDetailedView()}/>
+            }
+            {!showDetailedViewNonState &&
+                <img src="./src/assets/images/caret-left.png" className="accruals-bucket__show-detailed-view" onClick={() => toggleDetailedView()}/>
+            }
             <div class = "accruals-bucket__info-container">
-                    {bucketInfo.map((bucketName, value) => {
-                        <div class = {`accruals-bucket__row-${bucketName}`}>
-                            <div class={`accruals-bucket__row-${bucketName}-name-column`}>
-                                {bucketName}
+                    {bucketInfo.map((bucket, i) => 
+                        <div key={i} className={`accruals-bucket__row`}>
+                            <div class={`govuk-body-xs accruals-bucket__row-name-column`}>
+                                {bucket.bucketName}
                             </div>
-                            <div class={`accruals-bucket__row-${bucketName}-value-column`}>
-                                {value}
+                            <div class={`govuk-body-xs accruals-bucket__row-value-column`}>
+                                {bucket.value}
                             </div>
                         </div>
-                    })}
+                    )}
             </div>
         </div>
     )
 }
 
-export default Home;
+export default AccrualsBucket;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
+import AccrualsBucket from '../components/common/form/accruals-bucket/AccrualsBucket';
+
 const Home = () => {
-  return <h1 className="govuk-heading-xl">Home page</h1>;
+  return (
+    <div>
+      <h1 className="govuk-heading-xl">Home page</h1>
+      <AccrualsBucket bucketTitle="Hours Worked" bucketValue="2 Remaining" bucketInfo={[
+        {bucketName:"This Week", value:23},
+        {bucketName:"Next Week", value:42}
+      ]}/>
+    </div>
+  );
 };
 
 export default Home;
