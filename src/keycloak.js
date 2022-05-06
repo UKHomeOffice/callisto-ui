d047b0208e87fc31b6131bb47f995de388280f57
Holly Blues <holly.blues@digital.homeoffice.gov.uk>
2022-05-06 12:06:07 +0100

    EAHW-1859: Added /auth in kc url for v14/15 server

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,6 +1,6 @@
 import Keycloak from 'keycloak-js';
 const keycloak = new Keycloak({
-  url: 'http://localhost:8080/',
+  url: 'http://localhost:8080/auth',
   realm: 'callistorealm',
   clientId: 'callistoreactclientid',
 });
