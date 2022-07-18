d6e1ed723ec0351edac7fadc3c3f1379f3f53658
Seweryn Michota <Seweryn.Michota@digital.homeoffice.gov.uk>
2022-07-18 14:57:50 +0100

    Add renderWithTimecardContext test helper

--- src/test/Helpers.jsx
+++ src/test/Helpers.jsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import { ReactKeycloakProvider } from '@react-keycloak/web';
+import { TimecardContext } from '../pages/timecard/Timecard';
 
 export const renderWithProviders = (Component, AuthClientStub) =>
   render(
@@ -8,3 +9,19 @@ export const renderWithProviders = (Component, AuthClientStub) =>
       <BrowserRouter>{Component}</BrowserRouter>
     </ReactKeycloakProvider>
   );
+
+export const renderWithTimecardContext = (
+  Component,
+  values = {
+    summaryErrors: {},
+    setSummaryErrors: jest.fn(),
+  }
+) => {
+  return {
+    ...render(
+      <TimecardContext.Provider value={values}>
+        <MemoryRouter>{Component}</MemoryRouter>
+      </TimecardContext.Provider>
+    ),
+  };
+};
