8c48f24b472cc0cedf50e572864da60a8a6bb3d7
joannafawl <joanna.fawl@madetech.com>
2022-07-18 16:38:22 +0100

    Add dividers to summary box

--- src/components/timecard/edit-shift-timecard/EditShiftTimecard.jsx
+++ src/components/timecard/edit-shift-timecard/EditShiftTimecard.jsx
@@ -12,17 +12,27 @@ const EditShiftTimecard = () => {
 
   return (
     <div className="select-timecard-period-type">
-      <dl className="govuk-summary-list govuk-summary-list--no-border govuk-!-margin-bottom-0">
+      <dl className="govuk-summary-list govuk-!-margin-bottom-0">
         <div className="govuk-summary-list__row">
-          <dt className="govuk-summary-list__key">Shift</dt>
+          <dt
+            className="govuk-summary-list__key"
+            style={{ paddingBottom: '20px', paddingTop: '10px' }}
+          >
+            Shift
+          </dt>
           <dd className="govuk-summary-list__actions">
             <Link className="govuk-link govuk-link--no-visited-state" to={'/'}>
               Remove<span className="govuk-visually-hidden"> shift</span>
             </Link>
           </dd>
         </div>
-        <div className="govuk-summary-list__row">
-          <dt className="govuk-summary-list__key">Hours</dt>
+        <div className="govuk-summary-list__row govuk-summary-list__row--no-border">
+          <dt
+            className="govuk-summary-list__key"
+            style={{ paddingBottom: '20px', paddingTop: '20px' }}
+          >
+            Hours
+          </dt>
           <dd className="govuk-summary-list__actions">
             <Link
               onClick={toggleEditShiftHours}
@@ -37,8 +47,19 @@ const EditShiftTimecard = () => {
         {showEditShiftHours && (
           <EditShiftHours setShowEditShiftHours={setShowEditShiftHours} />
         )}
-        <div className="govuk-summary-list__row">
-          <dt className="govuk-summary-list__key">Meal break</dt>
+        <div
+          className="govuk-summary-list__row govuk-summary-list__row--no-border"
+          style={{ borderTop: '1px solid #b1b4b6' }}
+        >
+          <dt
+            className="govuk-summary-list__key"
+            style={{
+              paddingBottom: '10px',
+              paddingTop: '20px',
+            }}
+          >
+            Meal break
+          </dt>
           <dd className="govuk-summary-list__actions">
             <Link className="govuk-link govuk-link--no-visited-state" to={'/'}>
               Change<span className="govuk-visually-hidden"> meal break</span>
