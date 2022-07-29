2d6472a6eab1deab619ffb9e20f75fb53dd57365
Holly Blues <holly.blues@digital.homeoffice.gov.uk>
2022-07-29 16:20:24 +0100

    EAHW-1938: Change keycloak settings to env vars

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,8 +1,8 @@
 import Keycloak from 'keycloak-js';
 const keycloak = new Keycloak({
-  url: 'http://localhost:8080/auth',
-  realm: 'callistorealm',
-  clientId: 'callistoreactclientid',
+  url: import.meta.env.VITE_KC_URL,
+  realm: import.meta.env.VITE_KC_REALM,
+  clientId: import.meta.env.VITE_KC_CLIENTID,
 });
 
 export default keycloak;
