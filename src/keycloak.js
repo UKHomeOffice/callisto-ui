12ff29e0fd3507be4454b26dc7a36aecc4dcd2c5
Holly Blues <holly.blues@digital.homeoffice.gov.uk>
2022-08-01 10:15:05 +0100

    EAHW-1938: Change Keycloak to use env variables

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,4 +1,7 @@
 import Keycloak from 'keycloak-js';
-const keycloak = new Keycloak('/keycloak-config.json');
-
+const keycloak = new Keycloak({
+  url: import.meta.env.VITE_KC_URL,
+  realm: import.meta.env.VITE_KC_REALM,
+  clientId: import.meta.env.VITE_KC_CLIENTID,
+});
 export default keycloak;
