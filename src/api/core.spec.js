81c53c197e775e86c6b54198effb9713069bdafd
Holly Blues <holly.blues@digital.homeoffice.gov.uk>
2022-08-05 15:48:54 +0100

    EAHW-2126: Add tests for Core api

--- src/api/core.spec.js
+++ src/api/core.spec.js
@@ -2,10 +2,30 @@ import api from './core';
 
 jest.mock('./core');
 
-test('get returns data', async () => {
-  api.get.mockResolvedValue({ data: [] });
+describe('Core API', () => {
+  test('get returns data', async () => {
+    api.get.mockResolvedValue({ data: [] });
 
-  api.get('/api/timecard').then(function (request) {
-    expect(request).toBeDefined();
+    const response = await api.get('/api/test');
+
+    expect(response.data).toBeDefined();
+  });
+
+  test('post is successful', async () => {
+    api.post.mockResolvedValue({ data: [] });
+
+    const response = await api.post('/api/test', 'id=1', '{ id: 1}');
+
+    expect(response.data).toBeDefined();
+    expect(api.post).toHaveBeenCalledWith('/api/test', 'id=1', '{ id: 1}');
+  });
+
+  test('put is successful', async () => {
+    api.put.mockResolvedValue({ data: [] });
+
+    const response = await api.put('/api/test', '', '{ id: 2}');
+
+    expect(response.data).toBeDefined();
+    expect(api.put).toBeCalledWith('/api/test', '', '{ id: 2}');
   });
 });
