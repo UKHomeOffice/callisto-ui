53471f724637fa283c717f2c3212294f0053bc36
sewerynmi <seweryn.michota@digital.homeoffice.gov.uk>
2022-08-23 13:56:28 +0100

    Removing param state from URL on page reload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import { initAll } from 'govuk-frontend';
@@ -13,8 +13,15 @@ import { TimecardProvider } from './context/TimecardContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReactKeycloakProvider authClient={keycloak}>
-      <BrowserRouter>
+    <ReactKeycloakProvider
+      authClient={keycloak}
+      initOptions={{
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/silent-check-sso.html',
+      }}
+    >
+      <HashRouter>
         <Routes>
           <Route path="/" element={<App />}>
             <Route index="true" element={<Home />} />
@@ -45,7 +52,7 @@ ReactDOM.render(
             />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ReactKeycloakProvider>
   </React.StrictMode>,
   document.getElementById('root')
