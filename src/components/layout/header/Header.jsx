6f2d1a05d0fc6c3c5855398b9bd4fa99ba70a61f
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2022-08-23 11:53:56 +0100

    Minor re-alignment of logo and name

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
           className="govuk-header__container govuk-width-container"
           style={{ borderBottom: 0 }}
         >
-          <div className="govuk-header__logo govuk-!-margin-top-1">
+          <div className="govuk-header__logo govuk-!-margin-top-1 govuk-!-margin-bottom-0">
             <HomeOfficeLogo />
           </div>
           <div className="govuk-header__content">
