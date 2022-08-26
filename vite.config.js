7e7a309447446325f00023a998a4426f75adff2b
Holly Blues <holly.blues@digital.homeoffice.gov.uk>
2022-08-26 15:24:29 +0100

    EAHW-2126: Remove vite config url link

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig((config) => {
   process.env = { ...process.env, ...loadEnv(config.mode, process.cwd()) };
   const mockUrl = 'http://localhost:3001/';
