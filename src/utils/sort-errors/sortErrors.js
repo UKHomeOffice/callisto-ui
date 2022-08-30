26a5a06e2415827987e3feab2c0799f62b1949ec
laurie.bennett <laurie.bennett@digital.homeoffice.gov.uk>
2022-08-30 12:02:25 +0100

    EAHW-2149 fixed linting

--- src/utils/sort-errors/sortErrors.js
+++ src/utils/sort-errors/sortErrors.js
@@ -2,7 +2,7 @@ export const sortErrors = (errors, desiredOrder) => {
   var orderedKeys = [];
   const errorKeys = new Set(Object.keys(errors));
   desiredOrder.forEach((key) => {
-    if (errorKeys.has(key)){
+    if (errorKeys.has(key)) {
       orderedKeys.push(key);
     }
   });
