ba53f1d9cfc2e614ee1e520f1960b573dd6d7152
Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>
2022-08-31 10:29:03 +0100

    Individual tests passing but Suite failing

--- src/components/timecard/select-timecard-period-type/SelectTimecardPeriodType.spec.jsx
+++ src/components/timecard/select-timecard-period-type/SelectTimecardPeriodType.spec.jsx
@@ -1,5 +1,5 @@
 import { screen, fireEvent, waitFor } from '@testing-library/react';
-import { act } from 'react-test-renderer';
+// import { act } from 'react-test-renderer';
 import { renderWithTimecardContext } from '../../../test/helpers/TimecardContext';
 import * as timecardService from '../../../api/services/timecardService';
 
@@ -9,32 +9,34 @@ const mockRegister = jest.fn();
 const handleSubmit = jest.fn();
 const errors = {};
 const mockResponse = {
-  meta: {
-    next: null,
+  data: {
+    meta: {
+      next: null,
+    },
+    items: [
+      {
+        name: 'Shift',
+      },
+      {
+        name: 'Scheduled rest day',
+      },
+      {
+        name: 'Non-working day',
+      },
+      {
+        name: 'On call',
+      },
+      {
+        name: 'Absence',
+      },
+      {
+        name: 'Training',
+      },
+      {
+        name: 'Overtime',
+      },
+    ],
   },
-  items: [
-    {
-      name: 'Shift',
-    },
-    {
-      name: 'Scheduled rest day',
-    },
-    {
-      name: 'Non-working day',
-    },
-    {
-      name: 'On call',
-    },
-    {
-      name: 'Absence',
-    },
-    {
-      name: 'Training',
-    },
-    {
-      name: 'Overtime',
-    },
-  ],
 };
 const timePeriods = [
   'Shift',
@@ -65,13 +67,18 @@ describe('SelectTimecardPeriodType', () => {
       />
     );
 
-    timePeriods.map((option) => {
-      expect(screen.findByText(option)).toBeTruthy();
+    timePeriods.map(async (option) => {
+      let radioButton;
+      await waitFor(() => {
+        radioButton = screen.getByText(option);
+      });
+      expect(radioButton).toBeTruthy();
     });
   });
 
-  it('should display an error message when pressing submit with nothing selected', async () => {
-    renderWithTimecardContext(
+  it('should display an error message when pressing submit with nothing selected', () => {
+    mockGetTimePeriodTypes.mockResolvedValue(mockResponse);
+    const { getByText } = renderWithTimecardContext(
       <SelectTimecardPeriodType
         register={mockRegister}
         handleSubmit={handleSubmit}
@@ -79,14 +86,15 @@ describe('SelectTimecardPeriodType', () => {
       />
     );
 
-    act(() => {
-      const continueButton = screen.getByText('Continue');
-      fireEvent.click(continueButton);
-    });
+    const continueButton = getByText('Continue');
+    // act(() => {
+    fireEvent.click(continueButton);
+    // });
 
-    await waitFor(() => {
-      const errorMessage = screen.getByText('You must select a time period');
-      expect(errorMessage).toBeTruthy();
-    });
+    waitFor(() => getByText('You must select a time period')).then(
+      (errorMessage) => {
+        expect(errorMessage).toBeTruthy();
+      }
+    );
   });
 });
