b607fb63282950c794edcd504d8b84dbd2718d84
laurie.bennett <laurie.bennett@digital.homeoffice.gov.uk>
2022-09-13 22:51:39 +0100

    EAHW-1353 added unstyled modal

--- src/components/timecard/edit-shift-timecard/EditShiftTimecard.jsx
+++ src/components/timecard/edit-shift-timecard/EditShiftTimecard.jsx
@@ -6,12 +6,65 @@ import { deleteTimeEntry } from '../../../api/services/timecardService';
 import { UrlSearchParamBuilder } from '../../../utils/api-utils/UrlSearchParamBuilder';
 import { useTimecardContext } from '../../../context/TimecardContext';
 import { deepClone } from '../../../utils/common-utils/common-utils';
+import ModalUnstyled from '@mui/base/ModalUnstyled';
+import { styled, Box } from '@mui/system';
+import clsx from 'clsx';
+
+import * as React from 'react';
+
+const BackdropUnstyled = React.forwardRef((props, ref) => {
+  const { open, className, ...other } = props;
+  return (
+    <div
+      className={clsx({ 'MuiBackdrop-open': open }, className)}
+      ref={ref}
+      {...other}
+    />
+  );
+});
+
+const Modal = styled(ModalUnstyled)`
+  position: fixed;
+  z-index: 1300;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Backdrop = styled(BackdropUnstyled)`
+  z-index: -1;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  -webkit-tap-highlight-color: transparent;
+`;
+
+const style = (theme) => ({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  backgroundColor: theme.palette.mode === 'dark' ? '#0A1929' : 'white',
+  border: '2px solid currentColor',
+  boxShadow: 24,
+  padding: '16px 32px 24px 32px',
+});
 
 const EditShiftTimecard = ({ timeEntry, timeEntriesIndex }) => {
   const toggleEditShiftHours = (event) => {
     event.preventDefault();
     setShowEditShiftHours(!showEditShiftHours);
   };
+
+  const [open, setOpen] = useState(false);
 
   const { timeEntries, setTimeEntries } = useTimecardContext();
 
@@ -20,6 +73,13 @@ const EditShiftTimecard = ({ timeEntry, timeEntriesIndex }) => {
   const [showEditShiftHours, setShowEditShiftHours] = useState(
     !timeEntryIsEmpty
   );
+
+  const handleOpen = (event) => {
+    event.preventDefault();
+    setOpen(true);
+  };
+
+  const handleClose = () => setOpen(false);
 
   const clickRemoveShiftButton = async (event) => {
     event.preventDefault();
@@ -49,7 +109,7 @@ const EditShiftTimecard = ({ timeEntry, timeEntriesIndex }) => {
           <dd className="govuk-summary-list__actions" style={{ width: '10%' }}>
             {timeEntryIsEmpty && (
               <Link
-                onClick={clickRemoveShiftButton}
+                onClick={handleOpen}
                 className="govuk-link govuk-link--no-visited-state"
                 to={'/'}
               >
@@ -123,6 +183,29 @@ const EditShiftTimecard = ({ timeEntry, timeEntriesIndex }) => {
           </dd>
         </div>
       </dl>
+      <Modal
+        aria-labelledby="unstyled-modal-title"
+        aria-describedby="unstyled-modal-description"
+        open={open}
+        onClose={handleClose}
+        components={{ Backdrop }}
+      >
+        <Box sx={style}>
+          <h2 id="unstyled-modal-title">
+            Are you sure you want to remove this time period?
+          </h2>
+          <p id="unstyled-modal-description">
+            Once you remove it you cannot undo this action
+          </p>
+          <button
+            onClick={clickRemoveShiftButton}
+            className="govuk-button--link govuk-link"
+          >
+            Yes, remove it
+          </button>
+          <button className="govuk-button--link govuk-link">No, Cancel</button>
+        </Box>
+      </Modal>
     </div>
   );
 };
