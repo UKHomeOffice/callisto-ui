614c2a800c1a91e269d47c8b35d95500996d6bc4
daleelR <daleel.ramjan1@digital.homeoffice.gov.uk>
2022-09-14 17:00:14 +0100

    use template literal instead of string concatenation

--- src/utils/filters/resource-filter-builder/resourceFilterBuilder.js
+++ src/utils/filters/resource-filter-builder/resourceFilterBuilder.js
@@ -3,12 +3,12 @@ import { formatDateTimeISO } from '../../time-entry-utils/timeEntryUtils';
 
 export const filterOnOrAfterDate = (resourceDateProperty, date) => {
   const dateMidnight = formatDateTimeISO(dayjs(date).startOf('day'));
-  return resourceDateProperty + ">='" + dateMidnight + "'";
+  return `${resourceDateProperty}>='${dateMidnight}'`;
 };
 
 export const filterBeforeDate = (resourceDateProperty, date) => {
   const beforeDate = formatDateTimeISO(
     dayjs(date).startOf('day').subtract(1, 'minute')
   );
-  return resourceDateProperty + "<='" + beforeDate + "'";
+  return `${resourceDateProperty}<='${beforeDate}'`;
 };
