18a7e0324f6577c40894513dbc666e256e6ca426
Holly Blues <holly.blues@digital.homeoffice.gov.uk>
2022-10-10 16:35:47 +0100

    EAHW-2248: Temp to test Accruals connectivity

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,32 @@
 import { useEffect } from 'react';
+import { getHelloWorld } from '../api/services/accrualsService';
+import useApiCall from '../api/useApiCall';
 import generateDocumentTitle from '../utils/generate-document-title/generateDocumentTitle';
 
 const Home = () => {
   useEffect(() => {
     document.title = generateDocumentTitle('Home');
   });
+  const [loading, data, error] = useApiCall(getHelloWorld);
+
+  let apiResponse, apiError;
+  const loadingText = ' loading...';
+
+  if (error) {
+    apiError = error.message;
+  } else {
+    if (data) {
+      apiResponse = data.data;
+    }
+  }
 
   return (
     <>
       <h1 className="govuk-heading-xl">Home page</h1>
+
+      {loading && <p>{loadingText}</p>}
+      <p style={{ color: 'white' }}>{apiError}</p>
+      <p>{apiResponse}</p>
     </>
   );
 };
