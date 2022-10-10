6eff6923411b4bc11ec10028b25f7622ea7cdfb0
Holly Blues <holly.blues@digital.homeoffice.gov.uk>
2022-10-10 16:33:15 +0100

    EAHW-2248: Refactored to make simpler

--- vite.config.js
+++ vite.config.js
@@ -3,33 +3,23 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig((config) => {
   process.env = { ...process.env, ...loadEnv(config.mode, process.cwd()) };
+  const jsonServer = 'http://localhost:3001';
 
-  const viteServerUrl = 'http://localhost:3000/';
-
-  const localTimecardUrl = process.env['VITE_TIMECARD_API_URL'];
-  const localAccrualsUrl = process.env['VITE_ACCRUALS_API_URL'];
-
-  // Reset Vite local api urls to the same url and port the Vite Server is running on e.g. http://localhost:3000
-  // otherwise API requests will be made directly to the url specified e.g. http://localhost:3001 and not the Vite Server
-  // Vite Server is set up to proxy API requests based on the path requested e.g. http://localhost:3000/resources/time-entry maps to the timecardConfig (configured below in server > proxy)
-  Object.keys(process.env).forEach((key) => {
-    if (key.startsWith('VITE_') && key.indexOf('API_URL') > -1) {
-      process.env[key] = viteServerUrl;
-    }
-  });
-
-  const getUrl = (proxyToLocalUrl) => {
-    return proxyToLocalUrl;
+  const getUrl = (apiName) => {
+    const keyName = 'VITE_' + apiName + '_API_URL_PROXY';
+    const proxyToUrl = process.env[keyName] ? process.env[keyName] : jsonServer;
+    console.log('Proxying ' + apiName + ' to: ' + proxyToUrl);
+    return proxyToUrl;
   };
 
   const timecardConfig = {
-    target: getUrl(localTimecardUrl),
+    target: getUrl('TIMECARD'),
     changeOrigin: true,
     xfwd: true,
   };
 
   const accrualsConfig = {
-    target: getUrl(localAccrualsUrl),
+    target: getUrl('ACCRUALS'),
     changeOrigin: true,
     xfwd: true,
   };
@@ -51,7 +41,7 @@ export default defineConfig((config) => {
       proxy: {
         '/resources/time-period-types': timecardConfig,
         '/resources/time-entries': timecardConfig,
-        '/resources/accruals': accrualsConfig, // TODO: configure Accruals URLs accordingly
+        '/test': accrualsConfig, // TODO: configure Accruals URLs accordingly
       },
     },
   };
