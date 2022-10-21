e905cecf33e35a6179ebd06e99daba290fed50a3
laurie.bennett <laurie.bennett@digital.homeoffice.gov.uk>
2022-10-21 09:42:10 +0100

    Fixed linting issue

--- src/components/layout/header/SignIn.spec.jsx
+++ src/components/layout/header/SignIn.spec.jsx
@@ -34,42 +34,36 @@ describe('SignIn component', () => {
       logout: jest.fn(),
       authenticated: false,
       tokenParsed: {
-        "exp": 1666274108,
-        "iat": 1666273808,
-        "auth_time": 1666273808,
-        "jti": "f0713673-4795-40ee-a724-a13d595cbf6a",
-        "iss": "http://localhost:8080/auth/realms/callistorealm",
-        "aud": "account",
-        "sub": "9e47cf29-1598-4269-aa31-db1d2e4e0207",
-        "typ": "Bearer",
-        "azp": "callistoreactclientid",
-        "nonce": "faaae9ae-10f2-4845-a74f-a79af25e5d55",
-        "session_state": "65948f12-a8f3-461d-92a9-1d89ba8be9de",
-        "acr": "1",
-        "allowed-origins": [
-            "http://localhost:3000"
-        ],
-        "realm_access": {
-            "roles": [
-                "offline_access",
-                "default-roles-callistorealm",
-                "uma_authorization"
-            ]
+        exp: 1666274108,
+        iat: 1666273808,
+        auth_time: 1666273808,
+        jti: 'f0713673-4795-40ee-a724-a13d595cbf6a',
+        iss: 'http://localhost:8080/auth/realms/callistorealm',
+        aud: 'account',
+        sub: '9e47cf29-1598-4269-aa31-db1d2e4e0207',
+        typ: 'Bearer',
+        azp: 'callistoreactclientid',
+        nonce: 'faaae9ae-10f2-4845-a74f-a79af25e5d55',
+        session_state: '65948f12-a8f3-461d-92a9-1d89ba8be9de',
+        acr: '1',
+        'allowed-origins': ['http://localhost:3000'],
+        realm_access: {
+          roles: [
+            'offline_access',
+            'default-roles-callistorealm',
+            'uma_authorization',
+          ],
         },
-        "resource_access": {
-            "account": {
-                "roles": [
-                    "manage-account",
-                    "manage-account-links",
-                    "view-profile"
-                ]
-            }
+        resource_access: {
+          account: {
+            roles: ['manage-account', 'manage-account-links', 'view-profile'],
+          },
         },
-        "scope": "openid profile email",
-        "sid": "65948f12-a8f3-461d-92a9-1d89ba8be9de",
-        "email_verified": false,
-        "preferred_username": "callistouser"
-    },
+        scope: 'openid profile email',
+        sid: '65948f12-a8f3-461d-92a9-1d89ba8be9de',
+        email_verified: false,
+        preferred_username: 'callistouser',
+      },
       createLogoutUrl: jest.fn(),
       createLoginUrl: jest.fn(),
     };
