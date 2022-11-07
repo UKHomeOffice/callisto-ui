0d56f3e2ffdca63b4fb373c049c6aef03eae6093
joannafawl <joanna.fawl@madetech.com>
2022-11-07 14:13:30 +0000

    Add tests for displaying time clashes

--- src/components/timecard/time-input-errors/TimeInputErrors.jsx
+++ src/components/timecard/time-input-errors/TimeInputErrors.jsx
@@ -1,21 +1,60 @@
 import { PropTypes } from 'prop-types';
+import {
+  formatDate,
+  formatTime,
+} from '../../../utils/time-entry-utils/timeEntryUtils';
 
-const TimeInputErrors = ({ clashingProperty }) => {
-  switch (clashingProperty) {
-    case 'startTime':
-      return <p>Your start time must not overlap with another time period</p>;
-    case 'endTime':
-      return <p>Your finish time must not overlap with another time period</p>;
-    default:
-      return (
-        <p>
-          Your start and finish times must not overlap with another time period
-        </p>
-      );
-  }
+const TimeInputErrors = ({ clashingProperty, clashes }) => {
+  const displayClashingProperty = () => {
+    switch (clashingProperty) {
+      case 'startTime':
+        return <p>Your start time must not overlap with another time period</p>;
+      case 'endTime':
+        return (
+          <p>Your finish time must not overlap with another time period</p>
+        );
+      default:
+        return (
+          <p>
+            Your start and finish times must not overlap with another time
+            period
+          </p>
+        );
+    }
+  };
+
+  const displayTimeClashes = () => {
+    const clash = clashes[0];
+    const startTime = Date.parse(clash.startTime);
+    const endTime = Date.parse(clash.endTime);
+    let text;
+    if (clash.timePeriodTypeId == '00000000-0000-0000-0000-000000000001') {
+      text = `You are already assigned to work from ${formatTime(
+        startTime
+      )} to ${formatTime(endTime)} on ${formatDate(startTime)}`;
+    } else {
+      text = `You are already assigned a scheduled rest day on ${formatDate(
+        startTime
+      )}`;
+    }
+    return <p>{text}</p>;
+  };
+
+  return (
+    <>
+      {displayClashingProperty()} {displayTimeClashes()}
+    </>
+  );
 };
 
 export default TimeInputErrors;
 TimeInputErrors.propTypes = {
   clashingProperty: PropTypes.string.isRequired,
+  clashes: PropTypes.arrayOf(
+    PropTypes.shape({
+      timePeriodTypeId: PropTypes.string,
+      startTime: PropTypes.string,
+      endTime: PropTypes.string,
+    })
+  ).isRequired,
 };
--- src/components/timecard/time-input-errors/TimeInputErrors.spec.jsx
+++ src/components/timecard/time-input-errors/TimeInputErrors.spec.jsx
@@ -2,30 +2,93 @@ import { render, screen } from '@testing-library/react';
 import TimeInputErrors from './TimeInputErrors';
 
 describe('TimeInputErrors', () => {
-  it('should display a single error for start time clash', () => {
-    render(<TimeInputErrors clashingProperty={'startTime'} />);
+  const singleShiftClash = [
+    {
+      timePeriodTypeId: '00000000-0000-0000-0000-000000000001',
+      startTime: '2022-11-07T08:00:00.000+00:00',
+      endTime: '2022-11-07T12:00:00.000+00:00',
+    },
+  ];
 
-    const clashingShiftError = screen.getByText(
-      'Your start time must not overlap with another time period'
-    );
-    expect(clashingShiftError).toBeTruthy();
+  const singleScheduledRestDayClash = [
+    {
+      timePeriodTypeId: '00000000-0000-0000-0000-000000000002',
+      startTime: '2022-11-07T00:00:00.000+00:00',
+      endTime: '2022-11-08T00:00:00.000+00:00',
+    },
+  ];
+
+  describe('Clashing properties', () => {
+    it('should display a single error for start time clash', () => {
+      render(
+        <TimeInputErrors
+          clashingProperty={'startTime'}
+          clashes={singleShiftClash}
+        />
+      );
+
+      const clashingShiftError = screen.getByText(
+        'Your start time must not overlap with another time period'
+      );
+      expect(clashingShiftError).toBeTruthy();
+    });
+
+    it('should display a single error for finish time clash', () => {
+      render(
+        <TimeInputErrors
+          clashingProperty={'endTime'}
+          clashes={singleShiftClash}
+        />
+      );
+
+      const clashingShiftError = screen.getByText(
+        'Your finish time must not overlap with another time period'
+      );
+      expect(clashingShiftError).toBeTruthy();
+    });
+
+    it('should display a single error for start and finish time clash', () => {
+      render(
+        <TimeInputErrors
+          clashingProperty={'startAndEndTime'}
+          clashes={singleShiftClash}
+        />
+      );
+
+      const clashingShiftError = screen.getByText(
+        'Your start and finish times must not overlap with another time period'
+      );
+      expect(clashingShiftError).toBeTruthy();
+    });
   });
 
-  it('should display a single error for finish time clash', () => {
-    render(<TimeInputErrors clashingProperty={'endTime'} />);
+  describe('Single time clashes', () => {
+    it('should display a single error for shift clash', () => {
+      render(
+        <TimeInputErrors
+          clashingProperty={'startTime'}
+          clashes={singleShiftClash}
+        />
+      );
 
-    const clashingShiftError = screen.getByText(
-      'Your finish time must not overlap with another time period'
-    );
-    expect(clashingShiftError).toBeTruthy();
-  });
+      const clashingShiftError = screen.getByText(
+        'You are already assigned to work from 08:00 to 12:00 on 2022-11-07'
+      );
+      expect(clashingShiftError).toBeTruthy();
+    });
 
-  it('should display a single error for start and finish time clash', () => {
-    render(<TimeInputErrors clashingProperty={'startAndEndTime'} />);
+    it('should display a single error for scheduled rest day clash', () => {
+      render(
+        <TimeInputErrors
+          clashingProperty={'startTime'}
+          clashes={singleScheduledRestDayClash}
+        />
+      );
 
-    const clashingShiftError = screen.getByText(
-      'Your start and finish times must not overlap with another time period'
-    );
-    expect(clashingShiftError).toBeTruthy();
+      const clashingShiftError = screen.getByText(
+        'You are already assigned a scheduled rest day on 2022-11-07'
+      );
+      expect(clashingShiftError).toBeTruthy();
+    });
   });
 });
