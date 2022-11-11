d98d08d87baec1853a1b6b078379b604c0a284c7
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2022-11-11 10:31:38 +0000

    remove bug from errorSummary

--- src/components/common/form/error-summary/ErrorSummary.jsx
+++ src/components/common/form/error-summary/ErrorSummary.jsx
@@ -2,14 +2,6 @@ import PropTypes from 'prop-types';
 import { HashLink } from 'react-router-hash-link';
 
 const ErrorSummary = ({ errors, keys }) => {
-  const callbackRef = (summaryErrorList) => {
-    if (summaryErrorList) {
-      summaryErrorList.children['summary-error-0'].children[
-        'summary-error-0-message'
-      ];
-    }
-  };
-
   return (
     <div
       className="govuk-error-summary"
@@ -21,7 +13,7 @@ const ErrorSummary = ({ errors, keys }) => {
         There is a problem
       </h2>
       <div className="govuk-error-summary__body">
-        <ul className="govuk-list govuk-error-summary__list" ref={callbackRef}>
+        <ul className="govuk-list govuk-error-summary__list" ref={errors}>
           {keys.map((key, i) => (
             <li key={i} id={`summary-error-${i}`}>
               <HashLink id={`summary-error-${i}-message`} to={`#${key}`}>
