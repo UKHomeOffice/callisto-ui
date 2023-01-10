4ad6ecd68dda3cb96f5d38eccbf43d7a9ad026b1
joannafawl <joanna.fawl@madetech.com>
2023-01-10 14:23:54 +0000

    Add status conditions to notification steps

--- src/utils/time-entry-utils/timeEntryUtils.spec.js
+++ src/utils/time-entry-utils/timeEntryUtils.spec.js
@@ -50,7 +50,10 @@ describe('timeEntryUtils', () => {
       const startTime = '12:03';
       const finishTime = '00:00';
 
-      expect(isFinishTimeOnNextDay(startTime, finishTime)).toBeTruthy();
+      expect(isFinishTimeOnNextDay(startTime, finishTime)).toBeTruthy
+      
+      
+      ();
     });
 
     it('should return true when start time equals finish time', async () => {
