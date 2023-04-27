38255742ff45ccb893e20388781e23407f42943f
IanFisherHO <123647193+IanFisherHO@users.noreply.github.com>
2023-04-27 08:57:14 +0100

    Adding extra test to cover  handleAddTimePeriod()
    Adding extra test to cover  handleAddTimePeriod()

--- src/components/timecard/add-timecard-period/AddTimeCardPeriod.spec.jsx
+++ src/components/timecard/add-timecard-period/AddTimeCardPeriod.spec.jsx
@@ -3,6 +3,8 @@ import { screen, waitFor } from '@testing-library/react';
 import AddTimeCardPeriod from './AddTimeCardPeriod';
 import { addTimePeriodHeading } from '../../../utils/time-entry-utils/timeEntryUtils';
 import { renderWithApplicationContext } from '../../../test/helpers/TestApplicationContext';
+import { fireEvent } from '@testing-library/react';
+import { act } from 'react-test-renderer';
 
 describe('AddTimeCardPeriod component', () => {
   it('should display an add timecard period component', async () => {
@@ -12,6 +14,28 @@ describe('AddTimeCardPeriod component', () => {
       const addTimePeriodTitle = screen.queryByText(addTimePeriodHeading);
 
       expect(addTimePeriodTitle).toBeTruthy();
+    });
+  });
+
+  it('should call setNewTimeEntry when the add button is clicked', async () => {
+    const setNewTimeEntrySpy = jest.fn();
+    const setSummaryErrorsSpy = jest.fn();
+
+    renderWithApplicationContext(
+      <AddTimeCardPeriod
+        setAddNewTimeEntry={setNewTimeEntrySpy}
+        setSummaryErrors={setSummaryErrorsSpy}
+      />
+    );
+
+    act(() => {
+      const addTimeCardPeriodButton = screen.getByText('Add');
+      fireEvent.click(addTimeCardPeriodButton);
+    });
+
+    await waitFor(() => {
+      expect(setSummaryErrorsSpy).toHaveBeenCalledWith([]);
+      expect(setNewTimeEntrySpy).toHaveBeenCalledWith(true);
     });
   });
 });
