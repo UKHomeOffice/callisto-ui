2443be9dbbbdece12e361777dd73bca72ae90efb
IanFisherHO <123647193+IanFisherHO@users.noreply.github.com>
2023-05-09 15:36:46 +0100

    Calls working in order
    Calls working in order

--- src/pages/accruals/Accruals.jsx
+++ src/pages/accruals/Accruals.jsx
@@ -8,7 +8,7 @@ import dayjs from 'dayjs';
 import { UrlSearchParamBuilder } from '../../utils/api-utils/UrlSearchParamBuilder';
 import { validateServiceErrors } from '../../utils/api-utils/ApiUtils';
 import { buildAccrualsFilter, buildAgreementFilter, buildAgreementTargetFilter } from '../../utils/filters/accruals-filter/accrualsFilterBuilder';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getAccruals, getAgreements, getAgreementTargets } from '../../api/services/accrualsService';
 import AnnualTargetHours from '../../components/annual-target-hours/accruals/AnnualTargetHours';
 
@@ -18,17 +18,108 @@ const Accruals = () => {
   const previousDay = formatDate(dayjs(accrualsDate).subtract(1, 'day'));
   const nextDay = formatDate(dayjs(accrualsDate).add(1, 'day'));
 
-  const agreementStartDate = accrualsDate;
-  const agreementEndDate = accrualsDate;
+  const [agreementData, setAgreementData] = useState([]);
+  const [targetData, setTargetData] = useState([]);
+  const [accrualsData, setAccrualsData] = useState([]);
 
-  useEffect(() => {
-    getAgreementData(accrualsDate, setServiceError);
-    getAgreementTargetData(accrualsDate, setServiceError);
-    getAccrualsData(
-      accrualsDate,
-      setServiceError
-    );
+  let agreementStartDate = null;
+  let agreementEndDate = null;
+  let agreementId = '';
+
+  useEffect(async () => {
+    if (agreementStartDate === null || agreementEndDate === null) {
+      getAllData(accrualsDate, setServiceError);
+    }
+    else {
+      setAccrualsData(getAccrualsData(
+        accrualsDate,
+        setServiceError
+      ));
+    }
   }, [accrualsDate]);
+
+  const getAllData = async (
+    accrualsDate,
+    setServiceError
+  ) => {
+    await getAgreementData(accrualsDate, setServiceError);
+    if(agreementId) {
+      await getAgreementTargetData(accrualsDate, setServiceError);
+      await getAccrualsData(
+        accrualsDate,
+        setServiceError
+      );
+    }
+  };
+  
+  const getAgreementData = async (
+    date,
+    setServiceError
+  ) => {
+  const agreementParams = new UrlSearchParamBuilder()
+    .setTenantId('00000000-0000-0000-0000-000000000000')
+    .setFilter(buildAgreementFilter(date))
+    .getUrlSearchParams();
+  
+    await validateServiceErrors(setServiceError, async () => {
+      const agreementResponse = await getAgreements(agreementParams);
+  
+      if (agreementResponse.status == 200 && agreementResponse.data.items?.length > 0) {
+        console.log('Agreement data retrieved: ', agreementResponse.data.items);
+        agreementStartDate = agreementResponse.data.items[0].startDate;
+        agreementEndDate = agreementResponse.data.items[0].endDate;
+        agreementId = agreementResponse.data.items[0].id;
+        setAgreementData(agreementResponse.data.items);
+      }
+      else {
+        setAgreementData([]);
+      }
+    });
+  };
+  
+  const getAgreementTargetData = async (
+    date,
+    setServiceError
+  ) => {
+  const targetParams = new UrlSearchParamBuilder()
+    .setTenantId('00000000-0000-0000-0000-000000000000')
+    .setFilter(buildAgreementTargetFilter(agreementId))
+    .getUrlSearchParams();
+  
+    await validateServiceErrors(setServiceError, async () => {
+      const targetResponse = await getAgreementTargets(targetParams);
+  
+      if (targetResponse.status == 200 && targetResponse.data.items?.length > 0) {
+        console.log('Agreement target data retrieved: ', targetResponse.data.items);
+        setTargetData(targetResponse.data.items[0]);
+      }
+      else {
+        setTargetData([]);
+      }
+    });
+  };
+
+  const getAccrualsData = async (
+    date,
+    setServiceError
+  ) => {
+  const accrualsParams = new UrlSearchParamBuilder()
+    .setTenantId('00000000-0000-0000-0000-000000000000')
+    .setFilter(buildAccrualsFilter(date))
+    .getUrlSearchParams();
+  
+    await validateServiceErrors(setServiceError, async () => {
+      const accrualsResponse = await getAccruals(accrualsParams);
+  
+      if (accrualsResponse.status == 200 && accrualsResponse.data.items?.length > 0) {
+        console.log('Accruals data retrieved: ', accrualsResponse.data.items);
+        setAccrualsData(accrualsResponse.data.items);
+      }
+      else {
+        setAccrualsData([]);
+      }
+    });
+  };
  
   return (
     <>
@@ -55,60 +146,6 @@ const Accruals = () => {
       {/* <AnnualTargetHours /> */}
     </>
   );
-};
-
-const getAccrualsData = async (
-  date,
-  setServiceError
-) => {
-const accrualsParams = new UrlSearchParamBuilder()
-  .setTenantId('00000000-0000-0000-0000-000000000000')
-  .setFilter(buildAccrualsFilter(date))
-  .getUrlSearchParams();
-
-  validateServiceErrors(setServiceError, async () => {
-    const accrualsResponse = await getAccruals(accrualsParams);
-
-    if (accrualsResponse.status == 200) {
-      console.log('Accruals data retrieved: ', accrualsResponse.data.items);
-    }
-  });
-};
-
-const getAgreementData = async (
-  date,
-  setServiceError
-) => {
-const accrualsParams = new UrlSearchParamBuilder()
-  .setTenantId('00000000-0000-0000-0000-000000000000')
-  .setFilter(buildAgreementFilter(date))
-  .getUrlSearchParams();
-
-  validateServiceErrors(setServiceError, async () => {
-    const accrualsResponse = await getAgreements(accrualsParams);
-
-    if (accrualsResponse.status == 200) {
-      console.log('Agreement data retrieved: ', accrualsResponse.data.items);
-    }
-  });
-};
-
-const getAgreementTargetData = async (
-  date,
-  setServiceError
-) => {
-const accrualsParams = new UrlSearchParamBuilder()
-  .setTenantId('00000000-0000-0000-0000-000000000000')
-  .setFilter(buildAgreementTargetFilter('ac140007-87ea-1cbd-8187-ead0003a0000'))
-  .getUrlSearchParams();
-
-  validateServiceErrors(setServiceError, async () => {
-    const accrualsResponse = await getAgreementTargets(accrualsParams);
-
-    if (accrualsResponse.status == 200) {
-      console.log('Agreement target data retrieved: ', accrualsResponse.data.items);
-    }
-  });
 };
 
 export default Accruals;
