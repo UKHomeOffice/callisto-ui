4fb7d4e4b006da7957b83e93a8789c4f79b18422
IanFisherHO <123647193+IanFisherHO@users.noreply.github.com>
2023-05-12 14:47:18 +0100

    Adding mock data and tests
    Adding mock data and tests

--- src/pages/accruals/Accruals.spec.jsx
+++ src/pages/accruals/Accruals.spec.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, screen, render } from '@testing-library/react';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 import {
   renderWithApplicationContext,
   defaultApplicationContext,
@@ -6,10 +6,35 @@ import {
 import Accruals from './Accruals';
 import i18n from 'i18next';
 import '../../i18n';
+import { getAgreements, getAgreementTargets, getAccruals } from '../../api/services/accrualsService';
+import { agreement, agreementTarget, annualTargetHoursAccrual } from '../../../mocks/mockData';
 
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
 
 const t = i18n.getFixedT('en', 'common');
+
+jest.mock('../../api/services/accrualsService');
+beforeEach(() => {
+
+  getAgreements.mockImplementation(() => {
+    return {
+      status: 200,
+      data: agreement,
+    };
+  });
+  getAgreementTargets.mockImplementation(() => {
+    return {
+      status: 200,
+      data: agreementTarget,
+    };
+  });
+  getAccruals.mockImplementation(() => {
+    return {
+      status: 200,
+      data: annualTargetHoursAccrual,
+    };
+  });
+});
 
 describe('Accruals', () => {
 
@@ -34,6 +59,59 @@ describe('Accruals', () => {
       const nextDayLink = getByRole('link', { name: t('accruals.nextDay') });
       expect(nextDayLink.pathname).toBe('/accruals/2022-07-02');
       fireEvent.click(nextDayLink);
+    });
+  });
+
+  describe('getAcrrualsData', () => {
+    it('should retrieve all data when viewing a date in the agreement range', async () => {
+      const { getByRole, baseElement } = renderWithApplicationContext(<Accruals />,
+      defaultApplicationContext,
+      '/2023-04-01',
+      '/:date');
+      
+      await waitFor(async () => {
+        expect(
+          screen.getByText('Annual target hours 2182 remaining')
+        ).toBeTruthy();
+        expect(baseElement.innerHTML).toMatchSnapshot();
+      });
+    });
+
+    it('should retrieve no data when selecting next day outside the agreement range', async () => {
+      const { getByRole, baseElement } = renderWithApplicationContext(<Accruals />,
+      defaultApplicationContext,
+      '/2023-04-07',
+      '/:date');
+
+      // const nextDayLink = getByRole('link', { name: t('accruals.nextDay') });
+      // fireEvent.click(nextDayLink);
+      
+      await waitFor(async () => {
+        expect(
+          screen.getByText('No agreement has been found2')
+        ).toBeTruthy();
+        //expect(baseElement.innerHTML).toMatchSnapshot();
+      });
+    });
+
+    it('should retrieve no data when viewing a date outside the agreement range', async () => {
+      getAgreements.mockImplementation(() => {
+        return {
+          status: 200,
+          data: [],
+        };
+      });
+      const { getByRole, baseElement } = renderWithApplicationContext(<Accruals />,
+      defaultApplicationContext,
+      '/2023-04-08',
+      '/:date');
+      
+      await waitFor(async () => {
+        expect(
+          screen.getByText('No agreement has been found')
+        ).toBeTruthy();
+        expect(baseElement.innerHTML).toMatchSnapshot();
+      });
     });
   });
 });
