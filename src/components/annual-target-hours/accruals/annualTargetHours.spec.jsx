0f324ba9a37b42dcb044158097c04b37d2103202
Xeena-Newman <xeena.newman@digital.homeoffice.gov.uk>
2023-05-15 15:21:16 +0100

    change map to forEach

--- src/components/annual-target-hours/accruals/annualTargetHours.spec.jsx
+++ src/components/annual-target-hours/accruals/annualTargetHours.spec.jsx
@@ -13,51 +13,51 @@ describe('AnnualTargetHours', () => {
 
     const headers = ['Total', 'Worked', 'Remaining', 'Target'];
 
-    headers.map((header) => {
+    headers.forEach((header) => {
       expect(screen.getByText(header)).toBeTruthy();
     });
     expect(pretty(baseElement.innerHTML)).toMatchSnapshot();
   });
-});
 
-it('should display correct text for remaining hours with targetData', () => {
-  const targetData = { targetTotal: 131520 };
-  const accrualsData = { cumulativeTotal: 600 };
+  it('should display correct text for remaining hours with targetData', () => {
+    const targetData = { targetTotal: 131520 };
+    const accrualsData = { cumulativeTotal: 600 };
 
-  const { baseElement } = render(
-    <AnnualTargetHours targetData={targetData} accrualsData={accrualsData} />
-  );
+    const { baseElement } = render(
+      <AnnualTargetHours targetData={targetData} accrualsData={accrualsData} />
+    );
 
-  const expectedText = `Annual target hours 2182 remaining`;
+    const expectedText = `Annual target hours 2182 remaining`;
 
-  expect(screen.getByText(expectedText)).toBeTruthy();
-  expect(pretty(baseElement.innerHTML)).toMatchSnapshot();
-});
+    expect(screen.getByText(expectedText)).toBeTruthy();
+    expect(pretty(baseElement.innerHTML)).toMatchSnapshot();
+  });
 
-it('should display correct text for remaining hours without targetData', () => {
-  const targetData = null;
-  const accrualsData = { cumulativeTotal: 500 };
+  it('should display correct text for remaining hours without targetData', () => {
+    const targetData = null;
+    const accrualsData = { cumulativeTotal: 500 };
 
-  const { baseElement } = render(
-    <AnnualTargetHours targetData={targetData} accrualsData={accrualsData} />
-  );
+    const { baseElement } = render(
+      <AnnualTargetHours targetData={targetData} accrualsData={accrualsData} />
+    );
 
-  const expectedText = 'No agreement has been found';
+    const expectedText = 'No agreement has been found';
 
-  expect(screen.getByText(expectedText)).toBeTruthy();
-  expect(pretty(baseElement.innerHTML)).toMatchSnapshot();
-});
+    expect(screen.getByText(expectedText)).toBeTruthy();
+    expect(pretty(baseElement.innerHTML)).toMatchSnapshot();
+  });
 
-it('should display "-" for headers without targetData', () => {
-  const targetData = null;
-  const accrualsData = { cumulativeTotal: 100 };
+  it('should display "-" for headers without targetData', () => {
+    const targetData = null;
+    const accrualsData = { cumulativeTotal: 100 };
 
-  const { baseElement } = render(
-    <AnnualTargetHours targetData={targetData} accrualsData={accrualsData} />
-  );
+    const { baseElement } = render(
+      <AnnualTargetHours targetData={targetData} accrualsData={accrualsData} />
+    );
 
-  const expectedText = screen.getAllByText('-');
+    const expectedText = screen.getAllByText('-');
 
-  expect(expectedText.length).toBe(4);
-  expect(pretty(baseElement.innerHTML)).toMatchSnapshot();
+    expect(expectedText.length).toBe(4);
+    expect(pretty(baseElement.innerHTML)).toMatchSnapshot();
+  });
 });
