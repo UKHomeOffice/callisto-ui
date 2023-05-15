ed7f91bd4eeefe9b5a0be09804455a360570d559
Xeena-Newman <xeena.newman@digital.homeoffice.gov.uk>
2023-05-15 09:48:46 +0100

    update tests

--- src/components/annual-target-hours/accruals/annualTargetHours.spec.jsx
+++ src/components/annual-target-hours/accruals/annualTargetHours.spec.jsx
@@ -1,12 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import AnnualTargetHours from './AnnualTargetHours';
-import { initReactI18next } from 'react-i18next';
-import i18n from 'i18next';
-import { withTranslation, useTranslation } from 'react-i18next';
-import { I18nextProvider } from 'react-i18next';
 import '../../../i18n';
-
-const t = i18n.getFixedT('en', 'common');
+import pretty from 'pretty';
 
 describe('AnnualTargetHours', () => {
   it('should render a table with correct headers', () => {
@@ -17,48 +12,53 @@ describe('AnnualTargetHours', () => {
     const table = getByRole('table');
     expect(table).toBeTruthy();
 
-    expect(baseElement.innerHTML).toMatchSnapshot();
-  });
-});
+    const headers = ['Total', 'Worked', 'Remaining', 'Target'];
 
-it('should render a table with en-gb headers', () => {
-  const { getByRole } = render(
-    <AnnualTargetHours targetData={null} accrualsData={null} />
-  );
-
-  const table = getByRole('table');
-  expect(table).toBeTruthy();
-
-  const headers = ['Total', 'Worked', 'Remaining', 'Target'];
-
-  headers.map((header) => {
-    expect(screen.getByText(header)).toBeTruthy();
+    headers.map((header) => {
+      expect(screen.getByText(header)).toBeTruthy();
+    });
+    expect(pretty(baseElement.innerHTML)).toMatchSnapshot();
   });
 });
 
 it('should display correct text for remaining hours with targetData', () => {
-  const targetData = { targetTotal: 200 };
-  const accrualsData = { cumulativeTotal: 100 };
+  const targetData = { targetTotal: 131520 };
+  const accrualsData = { cumulativeTotal: 600 };
 
-  render(
+  const { baseElement } = render(
     <AnnualTargetHours targetData={targetData} accrualsData={accrualsData} />
   );
 
-  const remaining = targetData.targetTotal - accrualsData.cumulativeTotal;
-  const expectedText = `Annual target hours ${remaining} remaining`;
+  const expectedText = `Annual target hours 2182 remaining`;
 
   expect(screen.getByText(expectedText)).toBeTruthy();
+  expect(pretty(baseElement.innerHTML)).toMatchSnapshot();
 });
 
 it('should display correct text for remaining hours without targetData', () => {
   const targetData = null;
-  const accrualsData = { cumulativeTotal: 100 };
+  const accrualsData = { cumulativeTotal: 500 };
 
-  render(
+  const { baseElement } = render(
     <AnnualTargetHours targetData={targetData} accrualsData={accrualsData} />
   );
 
   const expectedText = 'No agreement has been found';
 
   expect(screen.getByText(expectedText)).toBeTruthy();
+  expect(pretty(baseElement.innerHTML)).toMatchSnapshot();
+});
+
+it('should display "-" for headers without targetData', () => {
+  const targetData = null;
+  const accrualsData = { cumulativeTotal: 100 };
+
+  const { baseElement } = render(
+    <AnnualTargetHours targetData={targetData} accrualsData={accrualsData} />
+  );
+
+  const expectedText = screen.getAllByText('-');
+
+  expect(expectedText.length).toBe(4);
+  expect(pretty(baseElement.innerHTML)).toMatchSnapshot();
 });
