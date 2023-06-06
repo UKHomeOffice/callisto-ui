71d11d7ec290f34b690d37c6b2e05ff938c048c7
Xeena-Newman <xeena.newman@digital.homeoffice.gov.uk>
2023-06-06 09:45:28 +0100

    add id and translation for weekend hours

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,4 +16,5 @@ export const messageKeys = Object.freeze({
 export const accrualsTypeIds = Object.freeze({
   annualTargetHours: 'e502eebb-4663-4e5b-9445-9a20441c18d9',
   nightHours: '5f06e6ce-1422-4a0c-89dd-f4952e735202',
+  weekendHours: '05bbd915-e907-4259-a2e2-080d7956afec',
 });
